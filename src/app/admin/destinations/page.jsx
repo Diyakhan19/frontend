702a0495ccb4107092cf2288598dee4eacb68545
Fayiz Ahmad <fayizkhan9@gmail.com>
2024-08-27 23:28:56 +0500

    Dest details page

--- src/app/admin/destinations/page.jsx
+++ src/app/admin/destinations/page.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Head from "next/head";
 import Image from "next/image";
 import Sidebar from "@/components/admin/Sidebar";
 import Topbar from "@/components/admin/Topbar";
